9a433da27df6722db894692ebf3ef812b32168e2
Jamila, Popi <gi6325@wayne.edu>
2021-02-07 21:23:00 -0500

    Adding author name

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,5 +1,5 @@
 // Simple C++ program to display "Hello World" 
-//Author: Amiangshu Bosu <amiangshu.bosu@wayne.edu>
+//Author: Jamila Popi <gi6325@wayne.edu>
   
 // Header file for input output functions 
 #include<iostream>  
