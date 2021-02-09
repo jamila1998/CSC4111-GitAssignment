54487f052c763f0f0bb1472406407217fb98891d
jamila1998 <78183336+jamila1998@users.noreply.github.com>
2021-02-08 19:08:11 -0500

    Update HelloWorld.cpp

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,5 +1,5 @@
 // Simple C++ program to display "Hello World" 
-//Author: Jamila Popi <gi6325@wayne.edu>
+//Author: Amiangsu Bosu <amiangsu.bosu@wayne.edu>
   
 // Header file for input output functions 
 #include<iostream>  
